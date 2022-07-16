5c00c1dbda3f0760b42af9e8733ed806c668cab9
Ou Chao Yuan <ou9810.dev@gmail.com>
2022-07-16 15:20:06 +0800

    Initial Commit

---
+++ DemoGraphQL/DemoGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGraphQLApp.swift
+//  DemoGraphQL
+//
+//  Created by Yuan on 2022/7/16.
+//
+
+import SwiftUI
+
+@main
+struct DemoGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
